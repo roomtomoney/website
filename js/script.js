e52ca2c07c745e2a4abefaf743f641a062f14176
HK Yoon <hkzuuu@gmail.com>
2015-07-25 04:09:41 +0900

    Jumbotron works

--- js/script.js
+++ js/script.js
@@ -64,3 +64,20 @@ Jumbo.prototype.start = function(interval) {
 	console.log(that);
 	this.setIntervalHandle = setInterval(this.next.bind(this), this.interval);
 };
+
+/* Smooth Scrolling */
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
